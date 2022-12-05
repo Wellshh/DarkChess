adffc0c4d6d26acd7e9664c5fcc71477966d339c
Victor0700 <1962075177@qq.com>
2022-12-05 17:34:08 +0800

    jygg3

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            StartScreen startScreen = new StartScreen(720, 720); //jygg!!!!
-            startScreen.setVisible(true); //jygg!!!!!
+            StartScreen startScreen = new StartScreen(720, 720);
+            startScreen.setVisible(true);
         });
     }
 }
